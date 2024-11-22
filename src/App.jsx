0365a3d29f7ebdc11c54de11cd80b9dd913785b5
Greybillions <olayile1999@gmail.com>
2024-11-23 00:03:32 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
